46ce4c7b41ab9f2c8accc39b58565ba11665f244
Enrico Zelioli <enricozelioli97@gmail.com>
2021-04-23 13:30:48 +0200

    timer driver

--- drivers/timer.c
+++ drivers/timer.c
@@ -2,16 +2,16 @@
 
 void timer_setup(unsigned int cmp_val){
 
-	unsigned int base_addr = timer_base_fc(0,0,0);
+	unsigned int base_addr = timer_base_fc(0,0);
 
 	timer_conf_set(base_addr, 0x00000094);
 	timer_reset(base_addr);
 	timer_cmp_set(base_addr, 0x00000020);
 }
 
-void timer_start(){ // start timer (preconfigured)... you should wait for interrupts now and receive them every 1ms
+void timer_start_timer(){ // start timer (preconfigured)... you should wait for interrupts now and receive them every 1ms
 
-	unsigned int base_addr = timer_base_fc(0,0,0);
+	unsigned int base_addr = timer_base_fc(0,0);
 
 	timer_start(base_addr);
 } // refactor with timer_timer_t struct or similar
